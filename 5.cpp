d6325b95d10454b7ebc5fa0db80ff084557bd95f
RK <karnriki70@gmail.com>
2025-05-17 18:26:57 +0545

    5th program

---
+++ 5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void circle(float r)
+{
+    const float pi = 3.1416;
+    float a = pi*r*r;
+    float p = 2*pi*r;
+    cout<<"Area of circle is:"<<a<<endl<<"Perimeter of circle is:"<<p<<endl; 
+}
+
+int main()
+{
+    float r1;
+    cout<<"Enter radius of circle:"<<endl;
+    cin>>r1;
+    circle(r1);
+    return 0;
+}
